d9f45943d89a0ea683cc4919e0b9da6adf30f0e0
Phumrapee Limpianchop <git@rayriffy.com>
2019-02-28 22:19:48 +0700

    :sparkles: Change ServiceWorker

--- public/sw.js
+++ public/sw.js
@@ -1,49 +1,47 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+var EXTRA_FILES = [];
 
-var version = 3;
+var CHECKSUM = "v1";
 
-workbox.setConfig({
-  debug: false
-})
+var FILES = [
+  '/',
+  'index.html',
+  'main.js',
+  'https://fonts.googleapis.com/css?family=Roboto:300,400',
+  'https://use.fontawesome.com/releases/v5.7.2/css/regular.css',
+  'https://use.fontawesome.com/releases/v5.7.2/css/brands.css',
+  'https://use.fontawesome.com/releases/v5.7.2/css/fontawesome.css',
+  'resources/NfJeVlcPAu0b7L3es4.jpg',
+  'resources/P64UlbFd9LK9aeUT3S.png',
+].concat(EXTRA_FILES || []);
 
-workbox.routing.registerRoute(/\.(?:html)$/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: 'html-cache-v' + version,
-  })
-);
+var CACHENAME = 'rayriffy-front-' + CHECKSUM;
 
-workbox.routing.registerRoute(
-  new RegExp('.*(?:googleapis|gstatic)\.com.*$'),
-  workbox.strategies.cacheFirst({
-    cacheName: 'googleapis-v' + version,
-    cacheExpiration: {
-      maxEntries: 30
-    },
-    cacheableResponse: {statuses: [0, 200]}
-  }),
-);
+self.addEventListener('install', function(event) {
+  event.waitUntil(caches.open(CACHENAME).then(function(cache) {
+    return cache.addAll(FILES);
+  }));
+});
 
-workbox.routing.registerRoute(
-  /\.(?:png|gif|jpg|jpeg|svg|ico)$/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: 'image-cache-v' + version,
-  })
-)
+self.addEventListener('activate', function(event) {
+  return event.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.map(function(k) {
+      if (k != CACHENAME && k.indexOf('rayriffy-front-') == 0) {
+        return caches.delete(k);
+      } else {
+        return Promise.resolve();
+      }
+    }));
+  }));
+});
 
-workbox.routing.registerRoute(
-  new RegExp('.*\.js'),
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: 'js-cache-v' + version,
-  })
-)
-
-workbox.routing.registerRoute(
-  new RegExp('.*\.css'),
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: 'css-cache-v' + version,
-  })
-)
-
-workbox.routing.setCatchHandler(() => {
-  return caches.match('/')
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response=>response||fetch(event.request))
+      .catch(() => {
+        if(event.request.mode == 'navigate') {
+          return caches.match('index.html');
+        }
+      })
+  );
 });
