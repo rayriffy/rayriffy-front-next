11835dfbea7e8c9094703ac5023bd7422ea63a26
Phumrapee Limpianchop <git@rayriffy.com>
2019-02-28 17:52:42 +0700

    :bug: Major fix

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,15 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
 
-var version = 2;
+var version = 3;
 
 workbox.setConfig({
   debug: false
 })
 
 workbox.routing.registerRoute(/\.(?:html)$/,
-  workbox.strategies.staleWhileRevalidate()
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'html-cache-v' + version,
+  })
 );
 
 workbox.routing.registerRoute(
@@ -21,17 +23,26 @@ workbox.routing.registerRoute(
   }),
 );
 
-workbox.routing.registerRoute('/resources/(.*)',
-  workbox.strategies.cacheFirst({
-    cacheName: 'images-cache-v' + version
-  })
-);
-
-workbox.routing.registerRoute('/\.(?:js|css)$/',
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg|ico)$/,
   workbox.strategies.staleWhileRevalidate({
-    cacheName: 'static-resources-v' + version
+    cacheName: 'image-cache-v' + version,
   })
-);
+)
+
+workbox.routing.registerRoute(
+  new RegExp('.*\.js'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'js-cache-v' + version,
+  })
+)
+
+workbox.routing.registerRoute(
+  new RegExp('.*\.css'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'css-cache-v' + version,
+  })
+)
 
 workbox.routing.setCatchHandler(() => {
   return caches.match('index.html')
