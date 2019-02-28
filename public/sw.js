6916f28d514a6dcdef8cab6af83f4d643e78e9d2
Phumrapee Limpianchop <git@rayriffy.com>
2019-02-28 17:39:22 +0700

    :bug: Minor fix

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
 
+var version = 2;
+
 workbox.setConfig({
   debug: false
 })
@@ -8,23 +10,33 @@ workbox.routing.registerRoute(/\.(?:html)$/,
   workbox.strategies.staleWhileRevalidate()
 );
 
-self.addEventListener('install', event => {
-  const urls = [
-    'https://fonts.googleapis.com/css?family=Roboto:300,400',
-    'https://use.fontawesome.com/releases/v5.7.2/css/regular.css',
-    'https://use.fontawesome.com/releases/v5.7.2/css/brands.css',
-    'https://use.fontawesome.com/releases/v5.7.2/css/fontawesome.css',
-    'resources/P64UlbFd9LK9aeUT3S.png',
-    'resources/NfJeVlcPAu0b7L3es4.jpg',
-    'main.js',
-    'index.html',
-    '/'
-  ];
-  const cacheName = 'rayriffy-v2';
-  event.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(urls))
-  );
-});
+workbox.router.registerRoute('https://fonts.googleapis.com/(.*)',
+  workbox.strategies.cacheFirst({
+    cacheName: 'googleapis-v' + version,
+    cacheExpiration: {
+      maxEntries: 30
+    },
+    cacheableResponse: {statuses: [0, 200]}
+  })
+);
+
+workbox.router.registerRoute(/\.(?:png|gif|jpg|svg)$/,
+  workbox.strategies.cacheFirst({
+    cacheName: 'images-cache-v' + version
+  })
+);
+
+workbox.router.registerRoute(/\.(?:js|css)$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'static-resources-v' + version
+  })
+);
+
+workbox.router.registerRoute('index.html',
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'homepage-resources-v' + version
+  })
+);
 
 workbox.routing.setCatchHandler(() => {
   return caches.match('index.html')
